fb36c594ea7de5fedd0c71bd5190c167acf400b7
Michael Benz <benz@heptacom.de>
2021-09-30 15:49:44 +0200

    Add ExceptionCodeLogger and changelog entry

---
+++ Contract/ForeignKeyAwareInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\Contract;
+
+interface ForeignKeyAwareInterface
+{
+    /**
+     * @psalm-return class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract>
+     */
+    public function getForeignEntityType(): string;
+
+    public function getForeignKey(): ?string;
+
+    public function setForeignKey(?string $foreignKey): void;
+}
