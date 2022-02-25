a314e7ad1ac5582f5690450d7ccd11a8e3ef59b3
Joshua Behrens <behrens@heptacom.de>
2022-02-25 21:57:49 +0100

    Use named reflection type instead of generic reflection type as string cast is deprecated

---
+++ AttachmentCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachableInterface;
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection;
+
+/**
+ * @extends AbstractObjectCollection<AttachableInterface>
+ */
+class AttachmentCollection extends AbstractObjectCollection
+{
+    protected function getT(): string
+    {
+        return AttachableInterface::class;
+    }
+}
---
+++ TaggedCollection/TaggedBooleanCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\TaggedCollection;
+
+use Heptacom\HeptaConnect\Dataset\Base\ScalarCollection\BooleanCollection;
+
+/**
+ * @extends AbstractTaggedCollection<bool>
+ */
+class TaggedBooleanCollection extends AbstractTaggedCollection
+{
+    /**
+     * @psalm-return BooleanCollection::class
+     */
+    protected function getCollectionType(): string
+    {
+        return BooleanCollection::class;
+    }
+}
