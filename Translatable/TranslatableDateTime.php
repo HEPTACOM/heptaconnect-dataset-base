57c28e6ddfd85cede7d810fcb08c339b40a317a1
Julian Krzefski <krzefski@heptacom.de>
2022-03-25 22:08:52 +0100

    CONNECT-25: Update documentation comments

---
+++ Translatable/TranslatableDateTime.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\Translatable;
+
+/**
+ * @extends AbstractTranslatable<\DateTimeInterface>
+ */
+final class TranslatableDateTime extends AbstractTranslatable
+{
+    protected function isValidValue($value): bool
+    {
+        /* @phpstan-ignore-next-line treatPhpDocTypesAsCertain checks soft check but this is the hard check */
+        return $value instanceof \DateTimeInterface;
+    }
+}
