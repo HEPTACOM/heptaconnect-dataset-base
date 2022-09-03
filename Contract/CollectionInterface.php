a13b5ab4ab8292007ad4a1c476bd8749508cc5b0
Joshua Behrens <behrens@heptacom.de>
2022-09-03 16:18:08 +0200

    Add collection reverse method
    CONNECT-572

---
+++ Contract/CollectionInterface.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\Contract;
+
+/**
+ * @template T
+ * @extends iterable<int, T>
+ * @extends \ArrayAccess<int, T>
+ * @extends \IteratorAggregate<int, T>
+ */
+interface CollectionInterface extends \IteratorAggregate, \Countable, \ArrayAccess, \JsonSerializable
+{
+    /**
+     * @psalm-param iterable<int, T> $items
+     */
+    public function push(iterable $items): void;
+
+    /**
+     * @return T|null
+     */
+    public function pop();
+
+    /**
+     * @return T|null
+     */
+    public function shift();
+
+    public function clear(): void;
+
+    /**
+     * @return T|null
+     */
+    public function first();
+
+    /**
+     * @return T|null
+     */
+    public function last();
+
+    /**
+     * @param callable(mixed):bool $filterFn
+     *
+     * @psalm-return \Generator<int, T>
+     */
+    public function filter(callable $filterFn): \Generator;
+
+    /**
+     * @psalm-param callable(T, array-key):mixed|callable(T):mixed $mapFn
+     *
+     * @return iterable<int, T>
+     */
+    public function map(callable $mapFn): iterable;
+
+    public function column(string $valueAccessor, ?string $keyAccessor = null): iterable;
+
+    /**
+     * Create a new collection of the same type, but without any content.
+     */
+    public function withoutItems(): self;
+
+    /**
+     * Group items in maximum $size big chunks. The last chunk can be less than $size items.
+     *
+     * @psalm-param positive-int $size
+     * @psalm-return iterable<self&non-empty-list<T>>
+     */
+    public function chunk(int $size): iterable;
+
+    /**
+     * Returns the items as a fixed size array. This is useful to use with methods that don't support iterables.
+     *
+     * @return array<T>
+     */
+    public function asArray(): array;
+
+    /**
+     * Returns the collection in reversed order.
+     *
+     * @return static
+     */
+    public function reverse(): self;
+}
---
+++ Translatable/ScalarCollection/TranslatableStringCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\Translatable\ScalarCollection;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\CollectionInterface;
+use Heptacom\HeptaConnect\Dataset\Base\ScalarCollection\StringCollection;
+
+/**
+ * @extends AbstractTranslatableScalarCollection<StringCollection>
+ */
+final class TranslatableStringCollection extends AbstractTranslatableScalarCollection
+{
+    protected function getInitialValue(): CollectionInterface
+    {
+        return new StringCollection();
+    }
+}
